cc13c63a7f4565fde5903fedbbca55b0bb71e2f9
SalehZo-Niloy <msnniloy7@gmail.com>
2022-11-24 23:11:38 +0600

    mongo client and user collection created

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.yarpj5v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        const userCollection = client.db('guitar-square').collection('users');
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            // console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Guitar Square running');
